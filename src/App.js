168fcde9d2c0f4ef74d582847728804d355d0646
Andra <andraelnn46@gmail.com>
2024-04-21 14:52:30 +0300

    works

--- src/App.js
+++ src/App.js
@@ -1,58 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Watermark from './components/Watermark';
 
-
 function App() {
-  const [image, setImage] = useState(null);
-  const [isWatermarkVisible, setIsWatermarkVisible] = useState(false);
+    const [image, setImage] = useState(null);
+    const [isWatermarkVisible, setIsWatermarkVisible] = useState(false);
+    const [encryptionKey, setEncryptionKey] = useState('');
 
-  // Download the image from the API and save it to a temporary file
-  const imageUrl = "https://api.unsplash.com/photos/KR2mdHJ5qMg";
-  const response = axios.get(imageUrl, { headers: { 'Authorization': 'Client-ID fhDwB0z_KpW2mQpxrrQmtaXEKE6IvZQqpU7kSOLILK8' } })
-    .then(response => setImage(response.data.urls.regular));
+    useEffect(() => {
+        // Descarcă imaginea de la API și o salvează într-un fișier temporar
+        const fetchImage = async () => {
+            try {
+                const imageUrl = "https://api.unsplash.com/photos/KR2mdHJ5qMg";
+                const response = await axios.get(imageUrl, { headers: { 'Authorization': 'Client-ID fhDwB0z_KpW2mQpxrrQmtaXEKE6IvZQqpU7kSOLILK8' } });
+                setImage(response.data.urls.regular);
+            } catch (error) {
+                console.error('Eroare la descărcarea imaginii:', error);
+            }
+        };
 
-  return (
-    <div style={{ display: 'grid', gap: '40px', gridTemplateColumns: 'repeat(2, 1fr)', width: '400px' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '20px' }}>
-        <button
-          style={{
-            background: "#FFB266",
-            border: "none",
-            color: 'white',
-            fontFamily: 'Poppins',
-            width: '150px',
-            height:'50px',
-            marginTop: '10px',
-            marginBottom: '10px',
-            fontSize:'20px',
-            borderRadius:'8px',
-            fontWeight:'600',
-            cursor:'pointer'
-          }}
-          onClick={() => {
-            setIsWatermarkVisible(true)
-          }
-          }>
-          Adauga Watermark
-        </button>
-        <img src={image} width={"700px"} />
-      </div>
-      {
-        image && (
-          <Watermark
-            imageUrl={image}
-            text="Compresia Datelor - Watermark"
-            fontColor="#ffffff"
-            fontSize="34px"
-            opacity={0.5}
-            isVisible={isWatermarkVisible}
-          />
-        )
-      }
+        fetchImage();
 
-    </div >
-  );
+        // Generarea cheii de criptare
+        generateEncryptionKey();
+    }, []);
+
+    // Funcția pentru generarea cheii de criptare
+    const generateEncryptionKey = async () => {
+        const key = await crypto.subtle.generateKey(
+            {
+                name: "AES-GCM",
+                length: 256,
+            },
+            true,
+            ["encrypt", "decrypt"]
+        );
+        const exportedKey = await crypto.subtle.exportKey("raw", key);
+        const keyHex = Array.from(new Uint8Array(exportedKey)).map(b => b.toString(16).padStart(2, '0')).join('');
+        console.log('Key ' + keyHex)
+        setEncryptionKey(keyHex);
+    };
+
+    return (
+        <div style={{ display: 'grid', gap: '40px', gridTemplateColumns: 'repeat(2, 1fr)', width: '400px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '20px' }}>
+                <button
+                    style={{
+                        background: "#FFB266",
+                        border: "none",
+                        color: 'white',
+                        fontFamily: 'Poppins',
+                        width: '150px',
+                        height:'50px',
+                        marginTop: '10px',
+                        marginBottom: '10px',
+                        fontSize:'20px',
+                        borderRadius:'8px',
+                        fontWeight:'600',
+                        cursor:'pointer'
+                    }}
+                    onClick={() => {
+                        setIsWatermarkVisible(true);
+                    }}
+                >
+                    Adaugă Watermark
+                </button>
+                <img src={image} width={"700px"} alt="Imagine" />
+            </div>
+            {
+                image && isWatermarkVisible && (
+                    <Watermark
+                        imageUrl={image}
+                        text="Compresia Datelor - Watermark"
+                        fontColor="#ffffff"
+                        fontSize="34px"
+                        opacity={0.5}
+                        isVisible={isWatermarkVisible}
+                        encryptionKey={encryptionKey} // Folosirea cheii generate
+                        metadata={{copyright: "COPYCEVA" }}
+                    />
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/components/Watermark.js
+++ src/components/Watermark.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import { createCanvas } from 'canvas';
+import CryptoJS from 'crypto-js'; // Asigură-te că ai importat CryptoJS
 
 const Watermark = ({
-  imageUrl,
-  text,
-  fontColor,
-  fontSize,
-  opacity,
-  isVisible,
-}) => {
+                     imageUrl,
+                     text,
+                     logoUrl,
+                     encryptionKey,
+                     metadata,
+                     isVisible,
+                   }) => {
   const canvasRef = useRef(null);
   const [watermarkImage, setWatermarkImage] = useState("");
 
@@ -26,28 +27,51 @@ const Watermark = ({
 
         ctx.drawImage(img, 0, 0);
 
-        ctx.fillStyle = fontColor;
-        ctx.font = `${fontSize} sans-serif`;
+        // Adăugarea textului watermark
+        ctx.fillStyle = '#000000';
+        ctx.font = '24px Arial';
+        ctx.fillText(text, 10, 30);
 
-        const x = img.width / 2 - ctx.measureText(text).width ;
-        const y = img.height  - Number(fontSize.replace("px", "")) / 2;
+        // Adăugarea logo-ului ca watermark
+        if (logoUrl) {
+          const logoImg = new Image();
+          logoImg.crossOrigin = "anonymous";
+          logoImg.src = logoUrl;
+          logoImg.onload = () => {
+            ctx.drawImage(logoImg, canvas.width - logoImg.width - 10, canvas.height - logoImg.height - 10);
+          };
+        }
 
-        ctx.fillText(text, x, y);  
-
-        const dataUrl = canvas.toDataURL("image/jpeg", opacity);
-        setWatermarkImage(dataUrl);
+        canvas.toBlob((blob) => {
+          const imageWithMetadata = new Image();
+          imageWithMetadata.src = URL.createObjectURL(blob);
+          imageWithMetadata.onload = () => {
+            console.log(metadata)
+            const encryptedData = encryptData(JSON.stringify(metadata), encryptionKey);
+            console.log(encryptedData);
+            // Presupunem că metadata este un obiect care trebuie convertit într-un șir
+            imageWithMetadata.metadata = encryptedData;
+            setWatermarkImage(imageWithMetadata.src);
+          };
+        }, 'image/jpeg', 1);
       };
     }
-  }, []);
+  }, [imageUrl, text, logoUrl, metadata, encryptionKey]); // Actualizarea dependențelor pentru useEffect
+
+  // Funcție pentru criptarea datelor
+  const encryptData = (data, key) => {
+    const encrypted = CryptoJS.AES.encrypt(data, key).toString();
+    return encrypted;
+  };
 
   return (
-    <img
-      ref={canvasRef}
-      style={{ opacity: isVisible ? 1 : 0 , marginTop:'70px'}}
-      src={watermarkImage}
-      width={"700px"}
-      alt=""
-    />
+      <img
+          ref={canvasRef}
+          style={{ opacity: isVisible ? 1 : 0, marginTop: '70px' }}
+          src={watermarkImage}
+          width={"700px"}
+          alt=""
+      />
   );
 };
 
